a54238c58504175d82d3926600cd20a7be0ffed4
ayushbhaimehta <ayushbhaimehta20002@gmail.com>
2023-01-09 01:43:53 +0530

    final stylign and refactoring before api work

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,23 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from "../components/banner/Banner"
 import NavBar from "../components/nav/navbar"
-import Card from "../components/card/card"
+// import Card from "../components/card/card"
 import Loading from "../components/loading/loading"
 import SectionCards from "../components/card/section-cards"
 
 
 export default function Home() {
+  const disneyVideos = [
+    {
+      imgUrl: "/static/clifford.webp",
+    },
+    {
+      imgUrl: "/static/clifford.webp",
+    },
+    {
+      imgUrl: "/static/clifford.webp",
+    },
+  ]
   return (
     <div className={styles.container}>
       <Head>
@@ -22,7 +33,10 @@ export default function Home() {
         subTitle="this is some random text"
         imgUrl="/static/clifford.webp"
       />
-      <SectionCards title="Disney" />
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+      </div>
       <Loading />
     </div>
   )
