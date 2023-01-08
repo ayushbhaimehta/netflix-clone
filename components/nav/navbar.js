11aec282c6daff7e31740f2aa00bc9e0c4e35b80
ayushbhaimehta <ayushbhaimehta20002@gmail.com>
2023-01-08 21:01:06 +0530

    navbar routing added

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,8 +1,24 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 import styles from "./navbar.module.css"
+import Link from 'next/link'
+
 
 const navbar = (props) => {
     const { username } = props;
+    const router = useRouter();
+    const handleOnclickHome = (e) => {
+        console.log("home clicked");
+        e.preventDefault()
+        router.push("/")
+    }
+
+    const handleOnclickMylist = (e) => {
+        console.log("My list clicked");
+        e.preventDefault()
+        router.push("/brouse/my-list")
+    }
+    console.log({ router });
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -10,8 +26,8 @@ const navbar = (props) => {
                     <div className={styles.logoWrapper}>Netflix</div>
                 </a>
                 <ul className={styles.navItems}>
-                    <li className={styles.navItem}>Home</li>
-                    <li className={styles.navItem2}>My List</li>
+                    <li className={styles.navItem} onClick={handleOnclickHome}>Home</li>
+                    <li className={styles.navItem2} onClick={handleOnclickMylist}>My List</li>
                 </ul>
                 <nav className={styles.navContainer}>
                     <div>
@@ -21,7 +37,9 @@ const navbar = (props) => {
 
                         <div className={styles.navDropdown}>
                             <div>
-                                <a className={styles.linkName}>Sign out</a>
+                                <Link href="/login" legacyBehavior>
+                                    <a className={styles.linkName}>Sign out</a>
+                                </Link>
                                 <div className={styles.lineWrapper}></div>
                             </div>
                         </div>
