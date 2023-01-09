df3947a3cf83ecfc8bf03caf7520a9f1a04540c1
ayushbhaimehta <ayushbhaimehta20002@gmail.com>
2023-01-09 17:01:13 +0530

    sign in page structure added

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from "../styles/login.module.css"
 
+const handleLoginwithEmail = (e) => {
+    console.log("login button clicked!");
+    e.preventDefault;
+}
+
 const login = () => {
     return (
         <div>
@@ -23,6 +28,19 @@ const login = () => {
                         </div>
                     </Link>
                 </div>
+                <main className={styles.main} >
+                    <div className={styles.mainWrapper}>
+                        <h1 className={styles.signinHeader} >
+                            Sign In
+                        </h1>
+                        <input
+                            type="text"
+                            placeholder='Email address'
+                            className={styles.emailInput} />
+                        <p className={styles.userMsg}></p>
+                        <button onClick={handleLoginwithEmail} className={styles.loginBtn} >Sign In </button>
+                    </div>
+                </main>
             </header>
         </div>
     )
