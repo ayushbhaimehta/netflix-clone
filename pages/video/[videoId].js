7024661d1908324459cb7410eeeae4b272ef33b8
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-11 03:23:22 +0530

    iframe settings added

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -17,9 +17,19 @@ const Video = () => {
                 className={styles.modal}
                 overlayClassName={styles.overlay}
             >
-                <div>Modal body</div>
-            </Modal>
-        </div>
+                <div>
+                    <iframe
+                        id="ytplayer"
+                        type="text/html"
+                        width="100%"
+                        height="360"
+                        src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+                        frameBorder="0"
+                    >
+                    </iframe>
+                </div>
+            </Modal >
+        </div >
     );
 };
 
