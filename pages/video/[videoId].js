0f384d5d4df3837292509280d7f17600dc732a38
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-11 03:28:13 +0530

    iframe styling done

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -19,6 +19,7 @@ const Video = () => {
             >
                 <div>
                     <iframe
+                        className={styles.videoPlayer}
                         id="ytplayer"
                         type="text/html"
                         width="100%"
