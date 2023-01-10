a818b98f654d0ac5d4a3391849855851e53ca5e5
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-11 03:09:24 +0530

    react modal styling added

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from 'next/router';
+import React from 'react';
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
 
@@ -9,12 +9,12 @@ const Video = () => {
     const router = useRouter();
     console.log({ router });
     return (
-        <div>
-            video page {router.query.videoId}
+        <div className={styles.container}>
             <Modal
                 isOpen={true}
                 contentLabel="Watch the video"
                 onRequestClose={() => router.back()}
+                className={styles.modal}
                 overlayClassName={styles.overlay}
             >
                 <div>Modal body</div>
