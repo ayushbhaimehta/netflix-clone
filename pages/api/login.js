0a9b7ea855f190e688d2ccbb12bcc0190b27d8de
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-12 02:40:20 +0530

    hasura checking user

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,27 +1,43 @@
-import { magicAdmin } from "../../lib/magic"
+import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
+import { isNewUser } from "../../lib/db/hasura";
 
-const login = async (req, res) => {
+export default async function login(req, res) {
     if (req.method === "POST") {
         try {
             const auth = req.headers.authorization;
-            const DIDToken = auth ? auth.substr(7) : "";
-            const metadata = await magicAdmin.users.getMetadataByToken(DIDToken);
-            // Get a token however you wish! Perhaps this is attached
-            // to `req.authorization`
-            // const DIDToken = // ...
+            const didToken = auth ? auth.substr(7) : "";
+            // invoke magic
 
-            // Retrieves user information by DID token
-            console.log({ auth }, { DIDToken }, { metadata });
-            res.send({ done: true })
+            const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+            console.log({ metadata });
+
+            // create jwt
+
+            const token = jwt.sign(
+                {
+                    ...metadata,
+                    iat: Math.floor(Date.now() / 1000),
+                    exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+                    "https://hasura.io/jwt/claims": {
+                        "x-hasura-allowed-roles": ["user", "admin"],
+                        "x-hasura-default-role": "user",
+                        "x-hasura-user-id": `${metadata.issuer}`,
+                    },
+                },
+                "thisisasecrettthisisasecrett1234"
+            );
+            console.log({ token });
+
+            // CHECK IF USER EXISTS
+
+            const isNewUserQuery = await isNewUser(token);
+            res.send({ done: true, isNewUserQuery });
         } catch (error) {
-            console.error("something is wrong");
-            res.status(500).send({ done: "falsesssss" })
+            console.error("Something went wrong logging in", error);
+            res.status(500).send({ done: false });
         }
-    }
-    else {
-        res.send({ done: false })
+    } else {
+        res.send({ done: false });
     }
 }
-
-export default login
-// "WyIweDM3NzZjYjhiZDM3ODMwNjQxYzNiZTljZTJiYzk2MzRhNGRhYmI4ZjA3NmU5ZGM0MjdkMTMyZThiMWM4N2U1N2E2NTAxOGYzZmUyZWJkMDAwZTZjMmFhMTA5ZTk1MjZlODc1NDU4YWI1YWRmNjdlMWE4MDE3NWU2MDNiZTg5MzRkMWIiLCJ7XCJpYXRcIjoxNjczNDY3MTQxLFwiZXh0XCI6MTY3MzQ2ODA0MSxcImlzc1wiOlwiZGlkOmV0aHI6MHhDMjA3NThEQzhmNjhFNkRmZGRjMzZGMWQwMjc1NDcyMkM3RkRFNzQxXCIsXCJzdWJcIjpcIl9RVkhwanJ6NHdDYzRPbE9tY2hTRXV4VXo0cFZqYklIdkkwY2pkYTNOdkE9XCIsXCJhdWRcIjpcIjBfZGNkdlFTWk9weFlYR0pPaFhZTzl3WVpJN0EwRnZHSHhyLVBKS3ZhdUE9XCIsXCJuYmZcIjoxNjczNDY3MTQxLFwidGlkXCI6XCIzMmUzMTQwMi1iZjRkLTQ5ODMtOWJhZC1lZDU3Mjc2ZjY1OGNcIixcImFkZFwiOlwiMHhlNGVhZmMwZmVmZGM0YWVjZTBiMzg2NzAxZjVhMDQ3MzFkMWE2MWQzYTE5MzM3NWRmMmE1MWJiYjY5ZDEyY2UzNjdmYzFjZDUzMTA5NWQ4NjA4ZTBmMDYwYTNmNDU5M2NjYjYxZjY3Y2E0N2U5ZmVlZGE1OGJhYjRmZDUzNDk2NDFiXCJ9Il0="
