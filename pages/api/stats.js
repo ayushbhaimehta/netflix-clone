1b7626f9e017e1b436a3260fcbf6bfe289bfbd56
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-15 14:12:35 +0530

    my list is yet to be added

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,26 +1,30 @@
 import jwt from "jsonwebtoken";
+import { findVideoIdByUser } from "../../lib/db/hasura"
 
-export default async function stats(req, resp) {
+export default async function stats(req, res) {
     if (req.method === "POST") {
         console.log({ cookies: req.cookies });
 
         try {
             const token = req.cookies.token
             if (!token) {
-                resp.status(403).send({});
+                res.status(403).send({});
             } else {
-                const decoded = jwt.verify(token, process.env.JWT_SECRET);
-                console.log({ decoded }) // bar
+                const videoId = req.query.videoId;
+                const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+                console.log({ decodedToken });
 
-                // verify a token symmetric
+                const userId = decodedToken.issuer;
+                const findVideoId = await findVideoIdByUser(token, userId, videoId);
+                console.log({ findVideoId });                // verify a token symmetric
                 // jwt.verify(token, 'shhhhh', function (err, decoded) {
                 //     console.log({decoded}) // bar
                 // });
-                resp.send({ msg: "it works" });
+                res.send({ msg: "it works", decodedToken, findVideoId });
             }
         } catch (error) {
             console.error("Error occurred /stats", error);
-            resp.status(500).send({ done: false, error: error?.message });
+            res.status(500).send({ done: false, error: error?.message });
         }
     }
 }
